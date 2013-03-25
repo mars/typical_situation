30731991fa663c0b9aab5740f40d677641de6a50
Mars <mars@abptinc.com>
2013-03-25 16:25:14 -0700

    Functional tests for :show

--- test/functional/mock_apple_pies_controller_test.rb
+++ test/functional/mock_apple_pies_controller_test.rb
@@ -18,11 +18,35 @@ class MockApplePiesControllerTest < ActionController::TestCase
   end
 
   test "GET index JSON" do
-    get :index, :format => 'json'
+    get :index, format: 'json'
     response_data = nil
     assert_nothing_thrown { response_data = JSON.parse(@response.body) }
     assert_kind_of Hash, response_data
     assert_kind_of Array, response_data['mock_apple_pies']
     assert_equal PIES_COUNT, response_data['mock_apple_pies'].size
+    response_data['mock_apple_pies'].each do |pie_data|
+      assert_equal @grandma.id, pie_data['grandma_id']
+      assert_not_nil pie_data['ingredients']
+    end
+  end
+
+  test "GET show" do
+    pie = @grandma.mock_apple_pies.first
+    get :show, id: pie.to_param
+    assert_response :success
+    assert_not_nil assigns(:mock_apple_pie)
+    assert_kind_of MockApplePie, assigns(:mock_apple_pie)
+    assert_template :show
+  end
+
+  test "GET show JSON" do
+    pie = @grandma.mock_apple_pies.first
+    get :show, id: pie.to_param, format: 'json'
+    response_data = nil
+    assert_nothing_thrown { response_data = JSON.parse(@response.body) }
+    assert_kind_of Hash, response_data
+    assert_kind_of Hash, response_data['mock_apple_pie']
+    assert_equal @grandma.id, response_data['mock_apple_pie']['grandma_id']
+    assert_not_nil response_data['mock_apple_pie']['ingredients']
   end
 end
