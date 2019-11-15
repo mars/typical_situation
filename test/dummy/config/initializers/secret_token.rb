544cb0e4cbdd8697a47e51fbf872d2ba293e4a71
Wyatt Kirby <wyatt@apsis.io>
2019-11-15 15:59:01 -0800

    Remove tests

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '7c33a89c12d001860a25a1cfb74a6572976c66198c85a4db3c85a585eb2c30fd887c55f23bda3c451169af03b996b1911ababfb50079d9f0eb7f75c9fd0cb82c'
